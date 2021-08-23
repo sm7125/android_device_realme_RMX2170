9dd92a8f5f706dd89af6d59e93298adebc0ed07d
SagarMakhar <sagarmakhar@gmail.com>
2021-08-23 20:02:21 +0000

    RMX2170: Switch to august build fp
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,7 @@ DEVICE_PATH := device/realme/RMX2170
 #TARGET_OTA_ASSERT_DEVICE := RMX2170
 
 # Build
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
-
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 
 BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtbs
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
