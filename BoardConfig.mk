c3528b33aa35ed39243af2868b448c01549448ae
SagarMakhar <sagarmakhar@gmail.com>
2021-09-21 03:02:42 +0000

    RMX2170: Enable assert on ota zip
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ include vendor/realme/RMX2170/BoardConfigVendor.mk
 DEVICE_PATH := device/realme/RMX2170
 
 # OTA assert
-#TARGET_OTA_ASSERT_DEVICE := RMX2170
+TARGET_OTA_ASSERT_DEVICE := RMX2170
 
 # Build
 TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
