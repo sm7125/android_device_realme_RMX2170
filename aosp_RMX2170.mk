d24f664a712d3de962bc9e50802048a5721ce460
SagarMakhar <sagarmakhar@gmail.com>
2021-11-09 06:10:35 +0000

    RMX2170: Disable gapps
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- aosp_RMX2170.mk
+++ aosp_RMX2170.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/realme/RMX2170/device.mk)
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-WITH_CORE_GAPPS := true
+#WITH_CORE_GAPPS := true
 EXTRA_FOD_ANIMATIONS := true
 
 EXTENDED_BUILD_TYPE := OFFICIAL
